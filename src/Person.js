1d4f09ab71207f883a579c7080d79f5482c3d489
AdamBilinski <adam.g.bilinski@gmail.com>
2018-11-26 21:34:41 +0000

    updated style

--- src/Person.js
+++ src/Person.js
@@ -4,7 +4,7 @@ import style from "./person.css";
 
 export function Person(){
     return(
-        <div className="person">
+        <div className="style.person">
             <h1>Adam</h1>
             <p>Your age is: 25</p>
         </div>
